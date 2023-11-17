4a6a7021d3ad487a70cdb0cd3183c8aaf5c05d9f
lynfan25702 <150467916+lynfan25702@users.noreply.github.com>
2023-11-17 18:41:00 +0700

    Create buổi 9.sql

---
+++ buổi 9.sql
@@ -0,0 +1,36 @@
+-- Ex1
+SELECT 
+sum (case 
+when device_type in ('tablet', 'phone') then 1 else 0 
+end) as mobile_views,
+sum (case 
+when device_type = 'laptop' then 1 else 0 
+end) as laptop_views
+FROM viewership;
+--Ex2
+select x,y,z, 
+case 
+when x+y>z and x+z>y and z+y>x then 'Yes'
+else 'No'
+end as triangle
+from triangle
+--Ex3
+
+--Ex4
+select name from customer
+where referee_id <> 2 or referee_id is null
+--Ex5
+select  
+case 
+when pclass =1 then 'first_class'
+when pclass=2 then 'second_class'
+when pclass=3 then 'third_class'
+end as class,
+sum (case
+when survived = 1 then 1 else 0
+end) as survivors,
+sum (case
+when survived = 0 then 1 else 0
+end) as non_survivors
+from titanic
+group by class

695cb95040a3c70fed79ae9d00007891aa78cc92
lynfan25702 <150467916+lynfan25702@users.noreply.github.com>
2023-12-06 11:47:05 +0700

    Update buổi 21.sql

--- buổi 21.sql
+++ buổi 21.sql
@@ -43,9 +43,9 @@ where gender ='F' and created_at between '2019-01-01 00:00:00' and '2022-05-01 0
 male_group as (select min (age) as youngest, max (age) as oldest
 from bigquery-public-data.thelook_ecommerce.users
 where gender ='M' and created_at between '2019-01-01 00:00:00' and '2022-05-01 00:00:00'
-)
+),
 
-select  c.first_name, c.last_name, c.gender, c.age
+age_group as (select  c.first_name, c.last_name, c.gender, c.age
 from bigquery-public-data.thelook_ecommerce.users as c join female_group as a
 on a.oldest=c.age or a.youngest=c.age
 where gender ='F' and created_at between '2019-01-01 00:00:00' and '2022-05-01 00:00:00'
@@ -53,7 +53,75 @@ Union all
 select  c.first_name, c.last_name, c.gender, c.age
 from bigquery-public-data.thelook_ecommerce.users as c join male_group as b
 on b.oldest=c.age or b.youngest=c.age
-where gender ='M' and created_at between '2019-01-01 00:00:00' and '2022-05-01 00:00:00'
+where gender ='M' and created_at between '2019-01-01 00:00:00' and '2022-05-01 00:00:00'),
+
+age_tag as (select *,
+case
+when age in (select min (age) from bigquery-public-data.thelook_ecommerce.users
+where gender ='M' and created_at between '2019-01-01 00:00:00' and '2022-05-01 00:00:00') then 'youngest'
+when age in (select  min (age) from bigquery-public-data.thelook_ecommerce.users
+where gender ='F' and created_at between '2019-01-01 00:00:00' and '2022-05-01 00:00:00' ) then 'youngest'
+else 'oldest'
+end as tag
+from age_group)
+
+select gender, tag, count (*) as customer_count
+from age_tag
+group by gender, tag
+
+/* Insights: 
+-Từ 1/2019-4/2022, ở mỗi nhóm nam lẫn nữ, số tuổi khách hàng lớn nhất đều là 70 và thấp nhất là 12.
+- So sánh giữa hai giới thì số lượng khách nam trẻ tuổi ít hơn số khách nữ trẻ tuổi và ngược lại nếu xét trong hạng mục độ tuổi lớn nhất của khách khi mua hàng. */
+
+/* 4. Thống kê top 5 sản phẩm có lợi nhuận cao nhất từng tháng (xếp hạng cho từng sản phẩm). 
+      Output: month_year ( yyyy-mm), product_id, product_name, sales, cost, profit, rank_per_month */
+
+with all_product as (select format_date ('%Y-%m', a.delivered_at) as month_year,
+a.product_id, b.name as product_name,
+round ((b.retail_price), 2) as sales,
+round ((b.cost),2) as cost,
+round ((b.retail_price - b.cost),2) as profit
+from bigquery-public-data.thelook_ecommerce.order_items as a
+join bigquery-public-data.thelook_ecommerce.products as b
+on a.id=b.id),
+
+top_product as (select * , dense_rank () over (partition by month_year order by profit DESC) as rank_per_month
+from all_product)
+
+select * from top_product
+where rank_per_month <6 and month_year is not null
+order by month_year
+
+/* 5. Thống kê tổng doanh thu theo ngày của từng danh mục sản phẩm (category) trong 3 tháng qua ( giả sử ngày hiện tại là 15/4/2022)
+      Output: dates (yyyy-mm-dd), product_categories, revenue */
+
+select format_date ('%Y-%m-%d', a.delivered_at) as dates,
+b.category as product_categories,
+round (sum(b.retail_price),2) as revenue
+from bigquery-public-data.thelook_ecommerce.order_items as a
+join bigquery-public-data.thelook_ecommerce.products as b
+on a.id=b.id
+where a.delivered_at between '2022-01-15' and '2022-04-15'
+group by b.category, dates 
+order by dates
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
  
